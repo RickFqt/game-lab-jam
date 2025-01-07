9cfc1de35297cd78202927731864b5346bc5f608
RickFqt <henriquefouquet@gmail.com>
2025-01-07 02:32:27 -0300

    feat: Balanced macaw

--- weapons/scripts/arara_circle.gd
+++ weapons/scripts/arara_circle.gd
@@ -4,16 +4,16 @@ extends Weapon
 func _ready() -> void:
 	attributes.base_cooldown = 2
 	attributes.base_damage = 2
-	attributes.base_speed = 100
+	attributes.base_speed = 150
 	attributes.n_projectiles = 1
 	attributes.base_area = 30.0
 	image = preload("res://textures/GUI/weapons/10_back.png")
 	descriptions= [
-		"Descricao arara",
-		"Descricao arara",
-		"Descricao arara",
-		"Descricao arara",
-		"Descricao arara"]
+		"Aumenta o dano base das araras em 2.",
+		"Diminui o tempo entre voos de araras em 33%.",
+		"Aumenta o dano base das araras em 2.",
+		"Aumenta a velocidade das araras em 33%.",
+		"Diminui o tempo entre voos de araras em 50%."]
 	weapon_name = "Arara"
 	%TimerArara.wait_time = calculate_cooldown()
 	%TimerArara.start()
@@ -34,15 +34,15 @@ func level_up(amount: int) -> void:
 	
 func _level_up() -> void:
 	if level == 1:
-		attributes.base_damage += 12
+		attributes.base_damage += 2.0
 	elif level == 2:
-		attributes.base_cooldown /= 2.0
+		attributes.base_cooldown /= 1.5
 	elif level == 3:
-		attributes.base_damage += 15
+		attributes.base_damage += 2.0
 	elif level == 4:
-		attributes.base_cooldown /= 2.0
-	elif level == 5:
 		attributes.base_speed *= 1.5
+	elif level == 5:
+		attributes.base_cooldown /= 2.0
 	level += 1
 
 func _on_timer_arara_timeout() -> void:
